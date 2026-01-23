c7057311854fb40bb804ac71b2d721ab7ce75356
EXM2 <Randaino2001@gmail.com>
2026-01-23 03:09:18 +0100

    QRPRO

--- sw.js
+++ sw.js
@@ -1,38 +1,17 @@
-const CACHE_NAME = "qrpro-cache-v1";
-const urlsToCache = [
-  "/EXM2/index.html",
-  "/EXM2/manifest.json",
-  "/EXM2/sw.js",
-  "/EXM2/icon-192.png",
-  "/EXM2/icon-512.png"
-];
-
-self.addEventListener("install", (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
+    caches.open('qrpro-cache').then(cache => {
+      return cache.addAll([
+        './index.html',
+        './manifest.json'
+      ]);
     })
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
+    caches.match(event.request)
+    .then(response => response || fetch(event.request))
   );
 });
