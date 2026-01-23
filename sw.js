6a8ecf293fa09bd97df0cc96323a07d49c1c3a92
RANDA099 <Randaino2001@gmail.com>
2026-01-23 01:22:23 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,38 @@
-self.addEventListener("install",e=>{
-e.waitUntil(
-caches.open("proqr").then(c=>c.addAll(["index.html"]))
-)
+const CACHE_NAME = "qrpro-cache-v1";
+const urlsToCache = [
+  "/EXM2/index.html",
+  "/EXM2/manifest.json",
+  "/EXM2/sw.js",
+  "/EXM2/icon-192.png",
+  "/EXM2/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
-self.addEventListener("fetch",e=>{
-e.respondWith(
-caches.match(e.request).then(r=>r||fetch(e.request))
-)
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 });
